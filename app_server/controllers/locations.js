315ad58a0c7b3f385d5ecc7c91961cf8dcc8ae73
Miguel Galindez <miguelgalindez@unicauca.edu.co>
2018-03-13 14:15:19 -0500

    Moving the data from the locations-list view to the respective controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,40 @@
 module.exports.homeList=function(req, res){
-	res.render('locations-list', {title: 'Home'});
+	res.render('locations-list', {
+		locations: [
+			{
+				name: 'StarCups',
+				address: '125 High Street, Reading, RG6 1PS',
+				rating: 3,
+				facilities: ['Hot drinks', 'Food', 'Premium Wifi'],
+				distance: '100m'
+			},
+			{
+				name: 'Cafe Hero',
+				address: '125 High Street, Reading, RG6 1PS',
+				rating: 4,
+				facilities: ['Hot drinks', 'Food', 'Premium Wifi'],
+				distance: '200m'
+			},
+			{
+				name: 'Burger Queen',
+				address: '125 High Street, Reading, RG6 1PS',
+				rating: 2,
+				facilities: ['Food', 'Premium wifi'],
+				distance: '250m'
+			},
+			{
+				name: 'Doña Rosa',
+				address: 'FIET- Unicauca',
+				rating: 5,
+				facilities: ['Empanadas', 'Tintos', 'Las hijas de doña rosa'],
+				distance: '40m'
+			}
+		]
+	});
 };
 
 module.exports.locationInfo=function(req, res){
-	res.render('location-info', {title: 'Location Info'});
+	res.render('location-info', {title: 'Home'});
 };
 
 module.exports.addReview=function(req, res){
