3ef905e02f0f14877af039bf809fe58d54c1c807
Miguel Angel <miguelgalindez@unicauca.edu.co>
2018-03-28 21:38:26 -0500

    Implemented new reusable function to put data from req.body into location object when it receives data from either POST or PUT method. Also, the locations controller's PUT methods are being developed.

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -4,67 +4,55 @@ var theEarth = require('./earth');
 
 var sendJsonResponse = function(res, status, content) {
 	res.status(status);
-	if(typeof content == 'string')
-		res.json({message: content});
+	if (typeof content == 'string')
+		res.json({
+			message: content
+		});
 	else
 		res.json(content);
 };
 
 module.exports.locationsCreate = function(req, res) {
-	Loc.create({
-		name: req.body.name,
-		address: req.body.address,
-		rating: req.body.rating,
-		facilities: req.body.facilities,
-		coords: [req.body.lng, req.body.lat],
-		openingTimes: [{
-			days: req.body.days1,
-			opening: req.body.opening1,
-			closing: req.body.closing1,
-			closed: req.body.closed1 == 'true'
-		},{
-			days: req.body.days2,
-			opening: req.body.opening2,
-			closing: req.body.closing2,
-			closed: req.body.closed2 == 'true'
-		}]
-	}, function(err, doc){
-		if(err)
-			sendJsonResponse(res, 400, err);
-		else
-			sendJsonResponse(res, 200, doc);
-	});
+	setRequestDataIntoLocation(req, {}, function(location){
+		Loc.create(location, function(err, doc) {
+			if (err)
+				sendJsonResponse(res, 400, err);
+			else
+				sendJsonResponse(res, 201, doc);
+		});	
+	});	
 };
 
-module.exports.locationsListByDistance=function(req, res){
+module.exports.locationsListByDistance = function(req, res) {
 	var lng = parseFloat(req.query.lng);
 	var lat = parseFloat(req.query.lat);
 	var maxDist = parseFloat(req.query.maxDist);
-	if(lng && lat){
-		var options={
-			near: [lng, lat],			
-			distanceField: 'dist.calculated',			
-			spherical: true,
-			limit:2
+	var limit=parseInt(req.query.limit);
+	if (lng && lat) {
+		var options = {
+			near: [lng, lat],
+			distanceField: 'dist.calculated',
+			spherical: true			
 		};
 
-		if(maxDist)
-			options.maxDistance=theEarth.getRadsFromDistance(maxDist);		
-		Loc.aggregate().near(options).exec(function(err, docs){
-			if(err)
+		if (maxDist)
+			options.maxDistance = theEarth.getRadsFromDistance(maxDist);
+		if(limit)
+			options.limit=limit;
+		Loc.aggregate().near(options).exec(function(err, docs) {
+			if (err)
 				sendJsonResponse(res, 404, err);
-			else{								
+			else {
 				processLocationsListByDistance(res, docs);
 			}
 		});
-	}
-	else
-		sendJsonResponse(res, 404, 'You must provide the longitude (lng) and latitude (lat).');	
+	} else
+		sendJsonResponse(res, 404, 'You must provide the longitude (lng) and latitude (lat).');
 };
 
-function processLocationsListByDistance(res, docs){
-	var locations=[];	
-	docs.forEach(function(location){
+function processLocationsListByDistance(res, docs) {
+	var locations = [];
+	docs.forEach(function(location) {
 		locations.push({
 			distance: theEarth.getDistanceFromRads(location.dist.calculated),
 			name: location.name,
@@ -77,51 +65,95 @@ function processLocationsListByDistance(res, docs){
 	sendJsonResponse(res, 200, locations);
 }
 
-module.exports.locationsReadOne=function(req, res){
-	if(req.params && req.params.locationID){
-		Loc.findById(req.params.locationID).exec(function(err, location){
-			if(err)
+module.exports.locationsReadOne = function(req, res) {
+	if (req.params && req.params.locationID) {
+		Loc.findById(req.params.locationID).exec(function(err, location) {
+			if (err)
 				sendJsonResponse(res, 404, err);
-			else{
-				if(location)
+			else {
+				if (location)
 					sendJsonResponse(res, 200, location);
 				else
 					sendJsonResponse(res, 404, "Location not found. Check out the location ID");
 			}
-		});	
-	}
-	else
+		});
+	} else
 		sendJsonResponse(res, 404, "No location ID in the request");
 };
 
-module.exports.locationsUpdateOne=function(req, res){
-	sendJsonResponse(res, 200, "Success");
+
+module.exports.locationsUpdateOne = function(req, res) {
+	if (req.params.locationID) {		
+		Loc.findById(req.params.locationID).select('-reviews -rating').exec(function(err, location) {
+			if (err)
+				sendJsonResponse(res, 400, err);
+			else if (!location)
+				sendJsonResponse(res, 404, "Location not found.");
+			else {				
+				setRequestDataIntoLocation(req, location, function(location){										
+					location.save(function(err, location) {
+						if (err)
+							sendJsonResponse(res, 400, err);
+						else
+							sendJsonResponse(res, 200, location);
+					});
+				});								
+			}
+		});
+	} else
+		sendJsonResponse(res, 400, "You must specify the location ID");
 };
-module.exports.locationsDeleteOne=function(req, res){
+
+
+module.exports.locationsDeleteOne = function(req, res) {
 	sendJsonResponse(res, 200, "Success");
 };
 
 
-module.exports.udpateAverageRating = function(locationID){
-	Loc.findById(locationID).select('rating reviews').exec(function(err, location){
-		if(!err)
+module.exports.udpateAverageRating = function(locationID) {
+	Loc.findById(locationID).select('rating reviews').exec(function(err, location) {
+		if (!err)
 			calculateAverageRating(location);
 	});
 };
 
-var calculateAverageRating = function(location){
+var calculateAverageRating = function(location) {
 	var i, reviewCount, ratingAverage, ratingTotal;
-	if(location.reviews && location.reviews.length>0){		
-		reviewCount=location.reviews.length;
-		ratingTotal = 0;		
+	if (location.reviews && location.reviews.length > 0) {
+		reviewCount = location.reviews.length;
+		ratingTotal = 0;
 		for (i = 0; i < reviewCount; i++) {
 			ratingTotal = ratingTotal + location.reviews[i].rating;
-		}		
-		ratingAverage=parseInt(ratingTotal/reviewCount, 10);
-		location.rating = ratingAverage;		
-		location.save(function(err, location){
-			if(err)
-				console.log("Error when trying to update the location rating value. "+JSON.stringify(err));
-		});		
+		}
+		ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+		location.rating = ratingAverage;
+		location.save(function(err, location) {
+			if (err)
+				console.log("Error when trying to update the location rating value. " + JSON.stringify(err));
+		});
 	}
+};
+
+var setRequestDataIntoLocation = function(req, location, callback) {		
+	for(var key in req.body)
+		location[key] = req.body[key] ? req.body[key] : location[key];
+
+	location.facilities = req.body.facilities ? req.body.facilities.replace(/\s{2,}/g, ' ').split(",").map(function(elem) {
+		return elem.trim();
+	}) : location.facilities;	
+
+	location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+
+	location.openingTimes = [{
+		days: req.body.days1,
+		opening: req.body.opening1,
+		closing: req.body.closing1,
+		closed: req.body.closed1,
+	}, {
+		days: req.body.days2,
+		opening: req.body.opening2,
+		closing: req.body.closing2,
+		closed: req.body.closed2,
+	}];	
+	callback(location);
 };
